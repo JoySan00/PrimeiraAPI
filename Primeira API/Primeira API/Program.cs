e176f05bb0aff0009badba39bfb07ba6c7a53409
roney <roney@gmail.com>
2025-01-23 15:11:02 -0300

    acho que to dando update-database, escrevi um código paracriar tabelas no banco de dados, e tive ainda que mudar as versões dos entity.frameworks e do Pomelo para ficarem compatíveis, dito isso, o banco de dados está criado bonitinho fiote

--- Primeira API/Primeira API/Program.cs
+++ Primeira API/Primeira API/Program.cs
@@ -1,4 +1,6 @@
+//program.cs
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Primeira_API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +11,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext> (options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConncetion"));
+// Adicionamos o serviço DbContext para que o Entity Framework Core saiba como gerenciar a conexão com o banco de dados
+builder.Services.AddDbContext<AppDbContext>(Options =>
+{   // Obtém a string de conexão definida no appsettings.json. 
+    // Aqui usamos o nome "DefaultConnection", que é a chave definida em "ConnectionStrings".
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    // Configura o Entity Framework Core para usar o provedor MySQL.
+    // O método UseMySql requer:
+    // 1. A string de conexão.
+    // 2. A versão do servidor MySQL para ajustar os recursos usados pelo Entity Framework Core.
+    // A string de conexão que contém informações como servidor, banco de dados, usuário e senha.
+    Options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 41)));
 });
 var app = builder.Build();
 
--- Primeira API/Primeira API/obj/Debug/net8.0/Primeira API.AssemblyInfo.cs
+++ Primeira API/Primeira API/obj/Debug/net8.0/Primeira API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Primeira API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cf8f52db6114e465c759cfa5d91a1d799c60be7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f96036a32ec53a0eab144d6000d4a963e4a5bde")]
 [assembly: System.Reflection.AssemblyProductAttribute("Primeira API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Primeira API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
